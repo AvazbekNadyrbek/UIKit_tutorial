a10ea56bcd7480dc234a4c2a1144afa32bd10077
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-05-07 15:35:43 +0200

    TextFiled Module

--- UIKit_tutorial/TextFields/TextFieldsViewController.swift
+++ UIKit_tutorial/TextFields/TextFieldsViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class TextFieldsViewController: UIViewController {
+    
+    private var textField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Search...."
+        tf.borderStyle = .roundedRect
+        tf.keyboardType = .emailAddress
+        tf.font = UIFont.systemFont(ofSize: 17)
+        tf.returnKeyType = .search
+        return tf
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        view.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        textField.widthAnchor.constraint(equalToConstant: view.frame.width - 64).isActive = true
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+#Preview {
+    TextFieldsViewController()
 }
