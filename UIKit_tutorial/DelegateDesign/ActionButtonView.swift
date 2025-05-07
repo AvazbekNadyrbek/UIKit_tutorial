b6eaa0e706b0636280911fdb5d82cb37159a690a
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-05-07 18:45:39 +0200

    Learnt a Design Pathertn named DELEGATE

---
+++ UIKit_tutorial/DelegateDesign/ActionButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ActionButtonView.swift
+//  UIKit_tutorial
+//
+//  Created by Авазбек Надырбек уулу on 07.05.25.
+//
+
+import UIKit
+
+class ActionButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
