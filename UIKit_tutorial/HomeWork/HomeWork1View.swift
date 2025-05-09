172041f47cdb0591879343f2e31014ad1f28dfec
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-05-09 12:18:42 +0200

    Выополил Домашку! Часть 1

---
+++ UIKit_tutorial/HomeWork/HomeWork1View.swift
@@ -0,0 +1,20 @@
+//
+//  HomeWork1View.swift
+//  UIKit_tutorial
+//
+//  Created by Авазбек Надырбек уулу on 08.05.25.
+//
+
+import UIKit
+
+class HomeWork1View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
